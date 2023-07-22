4433188cc25910009cb6244a7416a7ebdf9dbf81
pessolatohenrique <pessolatohenrique@gmail.com>
2023-07-22 19:12:02 -0300

    create validation to order

--- internal/entity/order.go
+++ internal/entity/order.go
@@ -1,5 +1,7 @@
 package entity
 
+import "errors"
+
 type Order struct {
 	ID         string
 	Price      float64
@@ -7,12 +9,35 @@ type Order struct {
 	FinalPrice float64
 }
 
-func NewOrder(id string, price float64, tax float64) *Order {
-	return &Order{
+func (o *Order) Validate() error {
+	if o.ID == "" {
+		return errors.New("field ID is required")
+	}
+
+	if o.Price < 0 {
+		return errors.New("field Price must be positive")
+	}
+
+	if o.Tax < 0 {
+		return errors.New("field Tax must be positive")
+	}
+
+	return nil
+}
+
+func NewOrder(id string, price float64, tax float64) (*Order, error) {
+	order := Order{
 		ID:    id,
 		Price: price,
 		Tax:   tax,
 	}
+	errors := order.Validate()
+
+	if errors != nil {
+		return nil, errors
+	}
+
+	return &order, nil
 }
 
 func (o *Order) CalculateFinalPrice() {
--- main.go
+++ main.go
@@ -6,10 +6,14 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello world")
-	order := entity.NewOrder("1", 50, 10)
-	order.CalculateFinalPrice()
-	fmt.Println(order)
-	fmt.Println(order.ID)
-	fmt.Println(order.FinalPrice)
+	order, err := entity.NewOrder("123", 50, 10)
+	if err == nil {
+		order.CalculateFinalPrice()
+		fmt.Println(order)
+		fmt.Println(order.ID)
+		fmt.Println(order.FinalPrice)
+	} else {
+		fmt.Println(err)
+	}
+
 }
