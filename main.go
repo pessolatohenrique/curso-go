e43601a71d2562065e4bcd7ac06c82798b9342de
pessolatohenrique <pessolatohenrique@gmail.com>
2023-07-27 20:09:54 -0300

    implementing concurrency with goroutines

--- main.go
+++ main.go
@@ -3,9 +3,47 @@ package main
 import (
 	"curso-go/internal/entity"
 	"fmt"
+	"sync"
+	"time"
 )
 
+func proccess(channel chan int, wg *sync.WaitGroup) {
+	for i := 0; i < 100000; i++ {
+		channel <- i
+	}
+	close(channel)
+	wg.Done()
+}
+
+func worker(channel chan int, wg *sync.WaitGroup, workerID int) {
+	defer wg.Done()
+	for {
+		fmt.Println("Received on channel", <-channel, " on workerID ", workerID)
+	}
+}
+
+func loopConcurrency() {
+	startTime := time.Now()
+
+	channel := make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go proccess(channel, &wg)
+	go worker(channel, &wg, 1)
+	go worker(channel, &wg, 2)
+	go worker(channel, &wg, 3)
+	go worker(channel, &wg, 4)
+	wg.Wait()
+
+	duration := time.Since(startTime)
+	fmt.Println("Duration of execution", duration)
+}
+
 func main() {
+	// concurrency
+	loopConcurrency()
+
+	// order
 	order, err := entity.NewOrder("123", 50, 10)
 	if err == nil {
 		order.CalculateFinalPrice()
